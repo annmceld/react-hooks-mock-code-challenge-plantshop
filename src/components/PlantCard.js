4c316b920e31c6a56879e8895be513b90b3dc4ed
Ann McElderry <thayerann1@gmail.com>
2023-04-19 11:44:15 -0500

    added update price form (only can console.log)

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -8,6 +8,17 @@ function PlantCard({plant, deleteThisPlant}) {
   const {id, name, image, price} = plant
 
   const [ inStock, setInStock ] = useState(true)
+  const [ formPrice, setFormPrice] = useState("")
+
+  function changeFormPrice (event) {
+    setFormPrice(event.target.value)
+  }
+
+  function submitNewPrice (event) {
+    event.preventDefault()
+    
+    console.log(formPrice)
+  }
 
   function toggleInStock () {
     setInStock(!inStock)
@@ -23,6 +34,10 @@ function PlantCard({plant, deleteThisPlant}) {
       <img src={image} alt={name} />
       <h4>{name}</h4>
       <p>Price: {price}</p>
+      <form onSubmit={submitNewPrice}>
+        <input type="number" placeholder="New Price" value={formPrice} onChange={changeFormPrice}/>
+        <button type="submit">Update Price</button>
+      </form>
       {inStock ? (
         <button className="primary" onClick={toggleInStock} >In Stock</button>
       ) : (
